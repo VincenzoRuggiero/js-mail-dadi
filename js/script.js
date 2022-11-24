ad0fe03960092325848365327a793732ae1e0e42
VincenzoRuggiero <vincenzoru@live.it>
2022-11-24 17:36:41 +0100

    Create email checklist

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@
 const dicePlayer1 = Math.floor(Math.random() * 6) + 1;
 const dicePlayer2 = Math.floor(Math.random() * 6) + 1;
 
+const score1 = document.getElementById('player1');
+const score2 = document.getElementById('player2');
+
 const vincitore = document.querySelector(".winner");
+score1.innerHTML = `${dicePlayer1}`;
+score2.innerHTML = `${dicePlayer2}`;
 
 if (dicePlayer1 > dicePlayer2) {
   vincitore.innerHTML = "Vince il Giocatore 1 ";
-} else if (dicePlayer2 > dicePlayer1) {
+
+} else if (dicePlayer1 < dicePlayer2) {
   vincitore.innerHTML = "Vince il Giocatore 2";
+
 } else if (dicePlayer1 === dicePlayer2) {
   vincitore.innerHTML = "Pareggio: nessun vincitore!";
 }
@@ -27,12 +34,18 @@ buttonElem.addEventListener("click", function () {
   const userMailElem = document.getElementById("userMail").value;
   const checkResult = document.querySelector(".check");
 
-  for (i = 0; i <= mailList.length; i++) {
-    if (userMailElem !== mailList.indexOf(i)) {
-      checkResult.innerHTML = "Ti sei registrato, benvenuto!";
+  let result;
+
+  for (i = 0; i < mailList.length; i++) {
+    if (userMailElem == mailList[i]) {
+      result = true;
+    }    
+
+    if (result){
+      checkResult.innerHTML = "Questa email è già presente nel sistema. Usane un'altra.";
     } else {
       checkResult.innerHTML =
-        "Questa email è già presente nel sistema. Usane un'altra.";
+        "Ti sei registrato, benvenuto!";
     }
   }
 });
